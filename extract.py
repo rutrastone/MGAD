f8eaa2faa587e7218c8f0ebef90c5f4c3c62efdb
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2017-09-30 19:38:48 +0200

    fixed extract

--- extract.py
+++ extract.py
@@ -1,63 +1,138 @@
-import sys, nltk
+import sys, nltk, pdb
 from itertools import product
 
+
 # core
-I01, I02, I03, I04, I05, I06, I07, I08, I09, I10 = [[] for i in range(10)]
+core = {}
+out = []
 
-# derivative
-I08, I09, I10 = [[], [], []]
 
-def check_tags(s, tags):
+template = """
+NOUN|Number=Plur    NOUN|Number=Sing
+"""
+
+template_ar = """
+NOUN|Number=Plur|Case=Nom|Definite=Ind   NOUN|Number=Sing|Case=Nom|Definite=Ind
+NOUN|Number=Plur|Case=Acc|Definite=Ind   NOUN|Number=Sing|Case=Acc|Definite=Ind
+NOUN|Number=Plur|Case=Gen|Definite=Ind   NOUN|Number=Sing|Case=Gen|Definite=Ind
+NOUN|Number=Sing|Case=Acc|Definite=Ind   NOUN|Number=Sing|Case=Nom|Definite=Ind
+NOUN|Number=Sing|Case=Gen|Definite=Ind   NOUN|Number=Sing|Case=Nom|Definite=Ind
+NOUN|Number=Sing|Case=Gen|Definite=Ind   NOUN|Number=Sing|Case=Acc|Definite=Ind
+VERB|Aspect=Imp|Gender=Masc|Person=3|Number=Sing    VERB|Aspect=Perf|Gender=Masc|Person=3|Number=Sing
+VERB|Aspect=Imp|Gender=Masc|Person=3|Number=Plur    VERB|Aspect=Perf|Gender=Masc|Person=3|Number=Plur
+VERB|Aspect=Imp|Gender=Masc|Person=3|Number=Sing    VERB|Aspect=Imp|Gender=Masc|Person=3|Number=Plur
+VERB|Aspect=Perf|Gender=Masc|Person=3|Number=Sing   VERB|Aspect=Perf|Gender=Masc|Person=3|Number=Plur
+"""
+
+template_ru = """
+NOUN|Number=Plur|Case=Nom   NOUN|Number=Sing|Case=Nom
+VERB|Person=3|Number=Sing|Tense=Pres    VERB|Aspect=Imp|VerbForm=Inf
+VERB|VerbForm=Part|Number=Sing|Case=Nom|Tense=Past|Gender=Masc  VERB|Aspect=Imp|VerbForm=Inf
+VERB|Number=Sing|Tense=Past|Gender=Masc VERB|Aspect=Imp|VerbForm=Inf
+VERB|Person=3|Number=Sing|Tense=Pres    VERB|VerbForm=Part|Number=Sing|Case=Nom|Tense=Past|Gender=Masc
+VERB|Person=3|Number=Sing|Tense=Pres    VERB|Number=Sing|Tense=Past|Gender=Masc
+VERB|VerbForm=Part|Number=Sing|Case=Nom|Tense=Past|Gender=Masc  VERB|Number=Sing|Tense=Past|Gender=Masc
+"""
+
+template = template_ar
+
+def check_tags(s, tags, disallow=[]):
     out = True
     for i in tags:
         out = out and i in s
-       
+    
+    for i in disallow:
+        out = out and i not in a
+
     return out
 
-def build_deriv(out, b1, b2):
+def build_deriv(b1, b2):
+    out = []
     for lemma, form in b1:
         try:
             out.append((form, dict(b2)[lemma]))
         except:
             pass
 
-for line in sys.stdin:
+    return out
+
+# read template
+for line in template.split("\n"):
+    if line == "":
+        continue
+    x, y = line.split()
+    core[x], core[y] = [], []
+
+
+lines = sys.stdin.readlines()
+sys.stdin = open('/dev/tty')
+
+for line in lines:
     if '#' in line:
         continue
 
     try:
         cols = line.split("\t")
-        w = cols[1]
+        word = cols[1]
+        lemma = cols[2] 
         pos = cols[3]
         feats = cols[5]
     except:
         continue
 
+    for i in core.keys():
+        t_pos, t_feats = i.split("|", maxsplit=1)
+        if pos == t_pos and check_tags(feats, t_feats.split("|")):
+            core[i].append((lemma, word))
+
+
+for line in template.split("\n"):
+    if line == "":
+        continue
+    x, y = line.split()
+    out.append(build_deriv(core[x], core[y]))    
+
+ 
+if sys.argv[1] == '--debug':
+    [print(len(i)) for i in out]
+
+else:
+    sys.stdout.write(": test\n")
+    freqs = [nltk.FreqDist(i) for i in out]
+    out = [[j[0] for j in i.most_common(50)] for i in freqs]
+
+    for I in out:
+        for i in product(I, I):
+            sys.stdout.write("{} {} {} {}\n".format(i[0][0], i[0][1], i[1][0],
+                                                    i[1][1]))
+   
+'''
     # regular plurals
-    if pos == 'NOUN' and check_tags(feats, ['Plur', 'Nom']):
+    if pos == 'NOUN' and check_tags(feats, ['Plur', 'Nom', 'Definite=Ind']):
         I01.append((cols[2], w))
 
-    # irregular plurals
-    I02 = []
+    # acc
+    if pos == 'NOUN' and check_tags(feats, ['Sing', 'Acc', 'Definite=Ind']):
+        I02.append((cols[2], w))
 
-    # comp
-    I03 = []
-    
-    # sup
-    I04 = []
+    # gen
+    if pos == 'NOUN' and check_tags(feats, ['Sing', 'Gen', 'Definite=Ind']):
+        I03.append((cols[2], w))
 
-    # inf
-    if pos == 'VERB' and check_tags(feats, ['Person=3', 'Sing', 'Pres']):
-        I05.append((cols[2], w))
+    # +3MSg+Impf : +3MSg+Perf
+    if pos == 'VERB' and check_tags(feats, ['Aspect=Imp', 'Gender=Masc',
+                                            'Person=3', 'Number=Sing',
+                                            'VerbForm=Fin'], ['Mood']):
+        I04.append((cols[2], w))
 
-    # pprs
-    if pos == 'VERB' and check_tags(feats, ['Part', 'Number=Sing', 'Case=Nom',
-                                            'Tense=Past', 'Gender=Masc']):
+    # +3MPl+Impf : 
+    if pos == 'VERB' and check_tags(feats, ['Aspect=Imp', 'Masc',
+                                            'VerbForm=Part', 'Sing',
+                                            'Person=3']):
         I06.append((cols[2], w))
 
     # past
-    if pos == 'VERB' and check_tags(feats, ['Number=Sing', 'Past',
-                                            'Gender=Masc']):
+    if pos == 'VERB' and check_tags(feats, ['VerbForm=Conv']):
         I07.append((cols[2], w))
 
 build_deriv(I08, I06, I05)
@@ -66,15 +141,4 @@ build_deriv(I10, I05, I07)
 
 final = [I01, I05, I06, I07, I08, I09, I10]
 
-if sys.argv[1] == '--debug':
-    [print(len(i)) for i in final]
-
-else:
-    sys.stdout.write(": test\n")
-    freqs = [nltk.FreqDist(i) for i in final]
-    final = [[j[0] for j in i.most_common(50)] for i in freqs]
-
-    for I in final:
-        for i in product(I, I):
-            sys.stdout.write("{} {} {} {}\n".format(i[0][0], i[0][1], i[1][0],
-                                                    i[1][1]))
+'''                                                    
